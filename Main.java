aed4dac288ed396f6745ce2d565ce54bed8857d7
ssailly <steven.sailly@gmail.com>
2023-12-13 17:02:16 +0100

    Parsed cmd arguments in the entry point of the app

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.io.File;
+
+public class Main {
+	public static void main(String[] args) {
+		String rdfFile = "", rdfsFile = "", sparqlFile = "";
+		String compliance = "default", output = "TURTLE";
+		boolean newfacts = false, debug = false;
+		for (String s : args) {
+			if (s.equals("debug")) debug = true;
+			else {
+				String[] arg = s.split("=");
+				switch (arg[0]) {
+					case "rdf" -> rdfFile = arg[1];
+					case "rdfs" -> rdfsFile = arg[1];
+					case "sparql" -> sparqlFile = arg[1];
+					case "compliance" -> compliance = arg[1];
+					case "output" -> output = arg[1];
+					case "newfacts" -> newfacts = true;
+					case "debug" -> debug = true;
+				}
+			}
+		}
+		if (rdfFile.isEmpty() || !new File(rdfFile).isFile()) {
+			throw new IllegalArgumentException("Invalid RDF file");
+		}
+		if (rdfsFile.isEmpty() || !new File(rdfsFile).isFile()) {
+			throw new IllegalArgumentException("Invalid RDFS file");
+		}
+		if (sparqlFile.isEmpty() || !new File(sparqlFile).isFile()) {
+			throw new IllegalArgumentException("Invalid SPARQL file");
+		}
+		if (debug) {
+			System.out.println("RDF file: " + rdfFile);
+			System.out.println("RDFS file: " + rdfsFile);
+			System.out.println("SPARQL file: " + sparqlFile);
+			System.out.println("Compliance: " + compliance);
+			System.out.println("New facts: " + newfacts);
+			System.out.println("Output: " + output);
+		}
+	}
+}
